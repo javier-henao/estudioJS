cddca138c5249738449bb1a0e27ac3eafa7cd085
javierhenaohe <javier.henao@outlook.com>
2024-12-30 10:59:30 -0500

    Fin seccion 2 - Actualizado

---
+++ assets/js/alerts.js
@@ -0,0 +1,13 @@
+// console.log(miNombre);
+
+// alert("Hola desde alerts.js");
+
+// let nombre = prompt("cual es tu nombre?", "sin nombre"); // prompt es una función que nos permite pedir información al usuario
+// console.log(nombre);
+
+// alert(`Hola ${nombre}`); // template string
+
+// let seleccion = confirm("esta seguro de borrar?");
+// console.log(seleccion);
+
+console.log(global);
---
+++ assets/js/app.js
@@ -0,0 +1,41 @@
+// console.log(console.log("Hola Mundo desde app.js"));
+
+// VARIABLE: Es un contenedor de información que guardamos en memoria y podemos reutilizarla en nuestro código.
+
+// TIPOS DE VARIABLES
+// var, let, const
+// let a = 10; // Variable que puede cambiar su valor     -- SE RECOMIENDA USAR,     -- forma moderna de declarar variables
+
+// var b = 10; // Variable que puede cambiar su valor     -- NO SE RECOMIENDA USAR,  -- forma antigua de declarar variables
+
+// const c = 10; // Variable que no puede cambiar su valor
+
+// c = 20; // ERROR -- NO SE PUEDE REASIGNAR
+
+/**************** */
+
+// alert("hola desde app.js");
+
+// console.log("Hola Mundo");
+
+let a = 10,
+  b = 20,
+  c = "Hola ",
+  d = "Spiderman",
+  x = a + b;
+
+const saludo = c + d; // concatenación de strings
+
+// console.log(saludo);
+
+// console.log("%c Mis variables", "color: green; font-weight: bold"); // %c -> Estilos de consola
+
+// console.log({ x });
+// console.log({ a });
+// console.log({ b });
+// console.log({ c });
+// console.log({ d });
+
+// console.table({ a, b, c, d, x }); // Muestra las variables en forma de tabla
+
+// var miNombre = "Javier";
